78dd984be5afc0bbadd6089287c8d1ac9ef48f0e
gretmn102 <erlgreywatson@gmail.com>
2026-07-04 15:32:56 +0300

    wip

---
+++ src/component/Card/Header.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { concat } from "../../lib/utils"
+  import { type Card } from "../../lib/card"
+
+  export let content: Card
+</script>
+
+<h2 class={concat([
+  "text-xl",
+  "text-center",
+])}>{content.title}</h2>
